e421cd67ff55c51f322161b31b3e01c73f49fd3a
VanessaGNF <vanney01@iba.dk>
2026-05-08 08:45:15 +0200

    last change in burger_menu.js

--- JS/burger_menu.js
+++ JS/burger_menu.js
@@ -1,3 +1,29 @@
+
+
+
+
+
 const barsButton = document.getElementById("bars");
 const subMenu = document.querySelector(".navig_burger");
 
+// Evento click al botón burger
+barsButton.addEventListener("click", () => {
+
+    // Obtener opacidad REAL desde CSS
+    const currentOpacity = getComputedStyle(subMenu).opacity;
+
+    // Si está oculto
+    if (currentOpacity === "0") {
+
+        subMenu.style.opacity = "1";
+
+        barsButton.setAttribute("aria-expanded", "true");
+
+    } else {
+
+        // Ocultar menú
+        subMenu.style.opacity = "0";
+
+        barsButton.setAttribute("aria-expanded", "false");
+    }
+});
